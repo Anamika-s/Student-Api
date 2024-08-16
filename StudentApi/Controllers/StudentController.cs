9f3b3a4bc64a03fa5edf0b6f01e9a1db71b3f0de
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-08-16 10:22:15 +0530

    Add project files.

---
+++ StudentApi/Controllers/StudentController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentApi.Models;
+
+namespace StudentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        // perform CRUD operations on Student Entity in Collection
+        static List<Student> students = null;
+        public StudentController()
+        {
+            if (students == null)
+            {
+                students = new List<Student>()
+                {
+                    new Student() { Id = 1, Name = "Ajay", Batch = "B001", Score = 90 },
+
+                    new Student() { Id = 2, Name = "Deepak", Batch = "B001", Score = 90 },
+
+                    new Student() { Id = 3, Name = "Chandan", Batch = "B001", Score = 90 },
+
+                    new Student() { Id = 4, Name = "Vijay", Batch = "B001", Score = 90 },
+                };
+            }
+        }
+
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return students;
+        }
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            foreach (var student in students)
+            {
+                if (student.Id == id)
+                {
+                    return student;
+                    break;
+                }
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        [HttpPut("{id}")]
+        public void EditStudent(int id, Student student)
+        {
+            Student temp = Get(student.Id);
+            if (temp != null)
+            {
+                temp.Batch = student.Batch;
+                temp.Score = student.Score;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteStudent(int id)
+        {
+            Student temp = Get(id);
+            if (temp != null)
+            {
+                students.Remove(temp);
+
+            }
+        }
+
+
+    }
+}
---
+++ StudentApi/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Batch { get; set; }
+        public int Score { get; set; }
+    }
+}
